5826bebdf50f925a7f87a4c1ed809ead41753755
ProElectro121 <94210870+ProElectro121@users.noreply.github.com>
2022-11-26 11:38:41 +0530

    Codepart.cpp

--- Code part.cpp
+++ Code part.cpp
@@ -35,14 +35,91 @@ int main() {
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//Inorder traversal
-void inorder(TreeNode* root , vector<int>& ans){
+//Recurrsive Preorder traversal
+
+void Preorder(TreeNode* root , vector<int>& ans){
         if(root == NULL)
             return;
         
-        ans.push_back(root -> val);
-        inorder(root -> left , ans);
-        inorder(root -> right , ans);
+        // Process the output
+    
+        // Calling left child
+        Preorder(root -> left , ans);
+    
+        // Calling right child
+        preorder(root -> right , ans);
 
 }
+
+//Recurrsive Inorder traversal
+
+ void inorder(TreeNode* root , vector<int> &ans){
+        if(root == NULL)
+            return;
+        
+        // Calling left child
+        inorder(root -> left , ans);
+     
+       //processing the output
+        ans.push_back(root -> val);
+     
+       // calling right child
+        inorder(root -> right , ans);
+ }
+
+
+
+//Recursive Postoder travesal
+
+
+ void Postorder(TreeNode* root , vector<int> &ans){
+        if(root == NULL)
+            return;
+        
+        // Calling left child
+        inorder(root -> left , ans);
+     
+       // calling right child
+        inorder(root -> right , ans);
+     
+       
+       //processing the output
+        ans.push_back(root -> val);
+     
+ }
+
+// Breath First Search traversal of Trees
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> BFS;
+        queue<TreeNode*> Q;
+        if(root != NULL)
+         Q.push(root);
+        while(!Q.empty()) {
+            int Queue_curr_size = Q.size();
+            vector<int> level;
+            for(int i = 0; i < Queue_curr_size; i++) {
+                
+                //accesing the top element and popping it
+                TreeNode* node = Q.front();
+                Q.pop();
+                
+                // Pushing left and right child respectively
+                if(node -> left != NULL)
+                    Q.push(node -> left);
+                if(node -> right != NULL)
+                    Q.push(node -> right);
+                
+                level.push_back(node -> val);
+            }
+            BFS.push_back(level);
+        }
+        return BFS;
+    }
+};
+
+
+
+
 
