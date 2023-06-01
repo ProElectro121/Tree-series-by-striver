88a8fee455b09c870f59a3b229c26b7c92167da2
ProElectro121 <94210870+ProElectro121@users.noreply.github.com>
2023-06-01 11:54:03 +0530

    Update BinarySearchTrees.cpp

--- BinarySearchTrees.cpp
+++ BinarySearchTrees.cpp
@@ -217,3 +217,115 @@ public:
         return ans;
     }
 };
+
+
+/*
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the
+lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL) return NULL;
+        int data_P = p -> val;
+        int data_q = q -> val;
+        int data = root -> val;
+        if(data_P > data and data_q > data) {
+            return lowestCommonAncestor(root -> right , p , q);
+        }
+        if(data_P < data and data_q < data) {
+            return lowestCommonAncestor(root -> left , p , q);
+        }
+        return root;
+    }
+};
+
+// Iterative version
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        int data_P = p -> val;
+        int data_q = q -> val;
+        while(root != NULL) {
+            int data = root -> val;
+            if(data > data_P and data > data_q) {
+                root = root -> left;
+            }
+            else if(data < data_P and data < data_q) {
+                root = root -> right;
+            }
+            else {
+                break;
+            }
+        }
+        return root;
+    }
+};
+
+
+/*
+1008. Construct Binary Search Tree from Preorder Traversal
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int ptr = 0;
+    TreeNode* bst(vector<int> preorder , long bound) {
+        if(ptr >= preorder.size() or preorder[ptr] > bound) return NULL;
+
+        TreeNode* root = new TreeNode(preorder[ptr++]);
+        root -> left = bst(preorder , root -> val);
+        root -> right = bst(preorder , bound);
+        return root;
+    }
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        return bst(preorder , LONG_MAX);
+    }
+};
+
+/*finding inorder succesor and predessor is simila to findinf floor and ceil in a BST
+
+for succesor --> IT is ceil 
+for predessor --> IT is floor
+*/
+
